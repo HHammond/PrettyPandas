1af37b92afdc370b658d38cde0719f5ab2bbd257
Henry Hammond <henryhhammond92@gmail.com>
2016-01-17 14:35:46 -0500

    bumped version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
 setup(
     name='prettypandas',
 
-    version='0.0.1',
+    version='0.0.1.post1',
 
     description='Pandas Styler for Report Quality Tables.',
     long_description=long_description,
