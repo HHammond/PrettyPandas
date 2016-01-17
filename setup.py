594847952fa2c43491824d519223ec8c80cf2c8a
Henry Hammond <henryhhammond92@gmail.com>
2016-01-17 14:26:35 -0500

    renamed file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'description.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
