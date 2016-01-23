62e555e93d3de3adc96a50862fbb4a7b864f86e6
Henry Hammond <henryhhammond92@gmail.com>
2016-01-23 14:52:32 -0500

    Added to root package

--- prettypandas/__init__.py
+++ prettypandas/__init__.py
@@ -1,4 +1,7 @@
 from .styler import apply_pretty_globals, PrettyPandas
+from .sparklines import Sparkline
 
 
-__all__ = ['PrettyPandas', 'apply_pretty_globals']
+__all__ = ['PrettyPandas',
+           'apply_pretty_globals',
+           'Sparkline']
